5f2ff8ccc5e030108356ca431c0a0d2d84eafd61
Jacobmd18 <jdalton12@huskers.unl.edu>
2021-08-21 16:45:49 +0000

    initial commit

---
+++ Hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jacob Dalton
+ * Date: 2021/08/20
+ *
+ * A simple hello world program in c
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Hello World! \n");
+
+    return 0;
+
+ }
